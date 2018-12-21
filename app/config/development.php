af21f7948c9d3705263d9e117f7815821ce5d6c7
TheSimonSays <thesupersimon@gmail.com>
2018-12-21 13:23:11 +0300

    'test'

---
+++ app/config/development.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+
+  'app' => [
+    'url' => 'http://localhost',
+    'hash' => [
+      'algorithm' => PASSWORD_BCRYPT,
+      'cost' => 10
+    ]
+  ],
+
+  'db' => [
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'name' => 'progroll',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => ''
+  ],
+
+  'auth' => [
+    'session_name' => 'user_id',
+    'remeber_cookie' => 'user_remember'
+  ],
+
+  'email' => [
+    'smtp_auth' => true,
+    'smtp_secure' => 'tls',
+    'host' => 'smtp.gmail.com',
+    'username' => 'thesupersimon@gmail.com',
+    'password' => '******',
+    'port' => 587,
+    'html' => true
+  ],
+
+  'twig' => [
+    'debug' => true
+  ],
+
+  'csrf' => [
+    'session' => 'csrf_token'
+  ]
+  
+];
